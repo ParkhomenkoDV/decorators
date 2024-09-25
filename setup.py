88473426ed6b65cee930f0d72ea3d91060d6dd8f
ParkhomenkoDV <parkho.m.enko@mail.ru>
2024-09-25 11:11:19 +0300

    2024-09-24-11-11

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt', 'rt') as file:
 
 setup(
     name=name,
-    version=time.strftime('%Y.%m.%d.%H.%M.%S', time.localtime()).split('.'),
+    version=time.strftime('%Y.%m.%d.%H.%M.%S', time.localtime()),
     description='lib',
     long_description=long_description,
     long_description_content_type='text/markdown',  # если long_description = .md
