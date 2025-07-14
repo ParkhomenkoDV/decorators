98e331a7335c3b9cbc53cae82125ca10ba35dcfa
AndryushinDaniilVitalevich <parkho.m.enko@mail.ru>
2025-07-14 21:38:20 +0300

    add test_deprecated

--- decorators/decorators_test.py
+++ decorators/decorators_test.py
@@ -1,6 +1,7 @@
+from colorama import Back
 import pytest
 
-from decorators import logger
+from decorators import logger, deprecated
 
 
 def test_logger(capsys):
@@ -30,3 +31,36 @@ def test_logger(capsys):
     assert add.__name__ == "add"
     assert add.__doc__ == "Складывает два числа."
     assert add.__module__ == "decorators.decorators_test"
+
+
+def test_deprecated(capsys):
+    test_message = "This function is deprecated"
+
+    @deprecated(test_message)
+    def test_func():
+        return 42
+
+    @deprecated("Some warning")
+    def sample_func(x: int, y: int) -> int:
+        """Sample function for testing"""
+        return x + y
+
+    # Тест проверяет вывод сообщения об устаревании
+    result = test_func()
+    captured = capsys.readouterr()
+    print(captured.out)
+    assert result == 42  # Проверяем, что функция работает
+    assert test_message in captured.out  # Проверяем сообщение
+    assert Back.RED in captured.out  # Проверяем цвет
+
+    # Тест проверяет сохранение метаданных функции
+    assert sample_func.__name__ == "sample_func"
+    assert sample_func.__doc__ == "Sample function for testing"
+    assert sample_func.__annotations__ == {"x": int, "y": int, "return": int}
+
+    # Тест проверяет, что декоратор вызывает ошибку при нестроковом сообщении
+    with pytest.raises(AssertionError):
+
+        @deprecated(123)  # type: ignore
+        def bad_func():
+            pass
