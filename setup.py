d4491b016e0bc137dd6e4a7197f9cb3b4ef418cc
AndryushinDaniilVitalevich <parkho.m.enko@mail.ru>
2025-07-28 11:26:41 +0300

    python>=3.9

--- setup.py
+++ setup.py
@@ -17,6 +17,6 @@ setup(
     author_email='parkho.m.enko@mail.ru',
     url='https://github.com/ParkhomenkoDV/decorators.git',
     packages=find_packages(),
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     install_requires=install_requires,
 )
